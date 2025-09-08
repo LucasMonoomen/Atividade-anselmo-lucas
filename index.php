a49f7f9df9ce1d58c6caadb511ec0f97802a6f7a
yagomonoyasuo <lukascspolar@gmail.com>
2025-09-08 10:40:19 -0300

    Agora eu comitei na conta correta
    correção de ponto e virgula e agora comitando com a conta correta do LucasMonoomen

--- index.php
+++ index.php
@@ -2,8 +2,12 @@
 
 echo 'bom dia anselmo';
 
-echo 'consegui comitar alguma coisa'
+echo 'consegui comitar alguma coisa';
 
-echo 'github é muito legal'
+echo 'github é muito legal';
+
+echo 'errei a conta agora o LucasMonoomen comitou de maneira eficiente';
+
+echo 'Permite que o Omen se teleporte para uma área selecionada após um breve tempo de canalização. Essa habilidade é versátil, usada para surpreender inimigos, chegar a locais elevados ou escapar de situações perigosas';
 
 ?>
